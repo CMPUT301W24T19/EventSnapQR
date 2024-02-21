65068bc5d15f0f6c2c5356ea8b1825aa1b48b404
mkrit419 <maurius@ualberta.ca>
2024-02-21 14:27:29 -0700

    added navigation

--- app/src/main/java/com/example/eventsnapqr/MainActivity.java
+++ app/src/main/java/com/example/eventsnapqr/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.eventsnapqr;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
 
@@ -10,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set up NavController
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        navController.navigate(R.id.mainPageFragment);
+        // Navigate to MainPageFragment
+
     }
+
 }
