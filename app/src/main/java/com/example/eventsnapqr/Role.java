df60c604871f83787feb91b6eaf17928623fbc2d
Ben S <benjaminstahlke@gmail.com>
2024-02-23 14:18:47 -0700

    Update Role.java

--- app/src/main/java/com/example/eventsnapqr/Role.java
+++ app/src/main/java/com/example/eventsnapqr/Role.java
@@ -1,2 +1,8 @@
-package com.example.eventsnapqr;public interface Role {
+package com.example.eventsnapqr;
+
+/**
+ * indicates that a class is capable of being assigned as a role
+ */
+public interface Role {
+    User getUser(); // returns the user associated the this instance of role
 }
