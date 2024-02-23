f5746b777a365867434f8ad810a0cd520a513a88
Ben S <benjaminstahlke@gmail.com>
2024-02-23 14:17:10 -0700

    Update Organizer.java

--- app/src/main/java/com/example/eventsnapqr/Organizer.java
+++ app/src/main/java/com/example/eventsnapqr/Organizer.java
@@ -2,11 +2,50 @@ package com.example.eventsnapqr;
 
 import java.util.List;
 
-public class Organizer {
-    private List<Event> myEvents;
-    public Organizer(){}
+/**
+ * an organizer that implements Role meaning it must
+ * be assigned as a Role with an associated user. an
+ * organizer is creating through the creation of one
+ * event, but may host more than one event.
+ */
+public class Organizer implements Role {
+    private User user; // the user associated to this attendee
+    private List<Event> organizedEvents;
+
+    /**
+     * constructor for organizer that must include a user and
+     * the event that resulted in the creation of this Organizer
+     * @param user associated user
+     * @param organizedEvents list of organized events
+     */
+    public Organizer(User user, Event organizedEvents) {
+        this.user = user;
+        this.organizedEvents.add(organizedEvents);
+    }
+
+    /**
+     * creates a new event
+     * @param qrCode
+     */
     private void createEvent(QR qrCode){
         Event newEvent = new Event(this, qrCode);
-        myEvents.add(newEvent);
+        organizedEvents.add(newEvent);
+    }
+
+    /**
+     * getter method for associated user
+     * @return this.user the associated user
+     */
+    public User getUser() {
+        return this.user;
+    }
+
+    /**
+     * getter method for list of events i the
+     * organizer have organized
+     * @return organized events
+     */
+    public List<Event> getOrganizedEvents() {
+        return organizedEvents;
     }
 }
