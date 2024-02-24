a7a8e346d90e10883170dc509f9212bb43ab9a1c
Nevin Xu <nevinxuregina@gmail.com>
2024-02-23 19:07:36 -0700

    Added navigation, fixed some xml fragment errors

---
+++ app/src/main/java/com/example/eventsnapqr/BrowseEventFragment.java
@@ -0,0 +1,36 @@
+package com.example.eventsnapqr;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class BrowseEventFragment extends Fragment {
+    private FloatingActionButton buttonBackButton;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_browse_events, container, false);
+        buttonBackButton = view.findViewById(R.id.button_back_button);
+        buttonBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment);
+                navController.navigate(R.id.action_browseEventFragment_to_mainPageFragment);
+            }
+        });
+        return view;
+    }
+}
