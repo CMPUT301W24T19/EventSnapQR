3b55dd45d5955cb01ab420cb37382a2885e58260
Joseph Al Fartosy <jmoneywayy@gmail.com>
2024-02-28 14:40:40 -0700

    Firebase controller setup

--- app/src/main/java/com/example/eventsnapqr/MainActivity.java
+++ app/src/main/java/com/example/eventsnapqr/MainActivity.java
@@ -14,11 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseController firebaseController = FirebaseController.getInstance();
 
         // Set up NavController
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         navController.navigate(R.id.mainPageFragment);
+        User newUser = new User("John Doe");
+        newUser.setHomepage("https://johndoe.com");
+        newUser.setContactInfo("john@example.com");
+        // Add the user to Firebase
+        firebaseController.addUser(newUser);
         // Navigate to MainPageFragment
+
 
     }
 
