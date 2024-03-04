552fb7318a31ce355e06796f8cb3e47baf24bdd9
WilliamGuo2002 <yg17@ualberta.ca>
2024-03-04 04:58:23 -0700

    Commited Changes

--- app/src/main/java/com/example/eventsnapqr/MyEventsFragment.java
+++ app/src/main/java/com/example/eventsnapqr/MyEventsFragment.java
@@ -1,5 +1,6 @@
 package com.example.eventsnapqr;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,12 +11,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
@@ -31,21 +28,28 @@ public class MyEventsFragment extends Fragment {
     private ArrayAdapter<String> eventAdapter;
     private List<String> eventNames;
     private FirebaseFirestore db;
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_my_events, container, false);
         attend_eventListView = v.findViewById(R.id.attending_events_list);
-        orgnize_eventListView = v.findViewById(R.id.organized_events_list);
+        orgnize_eventListView = v.findViewById(R.id.orgnized_events_list);
 
+        /*
+        * Should be replaced by entries from the database
+        * */
         eventNames = new ArrayList<>();
+        eventNames.add("Event 1");
+        eventNames.add("Event 2");
+        eventNames.add("Event 3");
+        eventNames.add("Event 4");
         eventAdapter = new ArrayAdapter<>(requireActivity(), android.R.layout.simple_list_item_1, eventNames);
         attend_eventListView.setAdapter(eventAdapter);
         orgnize_eventListView.setAdapter(eventAdapter);
 
         db = FirebaseFirestore.getInstance();
-        loadEvents();
 
         v.findViewById(R.id.button_back_button).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,20 +71,5 @@ public class MyEventsFragment extends Fragment {
         Bundle bundle = new Bundle();
         bundle.putString("eventName", eventName);
         navController.navigate(R.id.yourEventFragment, bundle);
-    }
-
-    private void loadEvents() {
-        db.collection("events").get()
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        eventNames.clear();
-                        for (DocumentSnapshot document : task.getResult()) {
-                            String eventName = document.getId();
-                            eventNames.add(eventName);
-                        }
-                        eventAdapter.notifyDataSetChanged();
-                    }
-                    else {} // error handling?
-                });
     }
 }
--- app/src/main/java/com/example/eventsnapqr/YourEventFragment.java
+++ app/src/main/java/com/example/eventsnapqr/YourEventFragment.java
@@ -1,15 +1,28 @@
 package com.example.eventsnapqr;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class YourEventFragment extends Fragment {
 
     private FirebaseController firebaseController;
+
+    private ListView attendeeListView;
+    private ArrayAdapter<String> eventAdapter;
+    private List<String> eventNames;
+    private FirebaseFirestore db;
 
     private String eventName;
     @Override
@@ -25,9 +38,26 @@ public class YourEventFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @SuppressLint("MissingInflatedId")
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_your_event, container, false);
 
+        attendeeListView = view.findViewById(R.id.attendee_list);
+        eventNames = new ArrayList<>();
+        eventNames.add("Attendee 1");
+        eventNames.add("Attendee 2");
+        eventNames.add("Attendee 3");
+        eventNames.add("Attendee 4");
+        eventAdapter = new ArrayAdapter<>(requireActivity(), android.R.layout.simple_list_item_1, eventNames);
+
+        attendeeListView.setAdapter(eventAdapter);
+
+        view.findViewById(R.id.button_back_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requireActivity().onBackPressed();
+            }
+        });
 
         return view;
     }
