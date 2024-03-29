705ca25ab307f6e0627a9cbd6d8237a06db2b9ce
Mjcs99 <maurius@ualberta.ca>
2024-03-28 20:25:28 -0600

    Organize events poster fix

--- app/src/main/java/com/example/eventsnapqr/OrganizeEventFragment.java
+++ app/src/main/java/com/example/eventsnapqr/OrganizeEventFragment.java
@@ -7,11 +7,19 @@ import android.app.TimePickerDialog;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Shader;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.LayerDrawable;
 import android.graphics.drawable.LevelListDrawable;
 import android.Manifest;
 import android.graphics.ImageDecoder;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RoundRectShape;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -148,14 +156,24 @@ public class OrganizeEventFragment extends Fragment {
                         try {
                             InputStream inputStream = requireContext().getContentResolver().openInputStream(uri);
                             Bitmap originalBitmap = BitmapFactory.decodeStream(inputStream);
-                            int targetWidth = 500;
-                            int targetHeight = 500;
-                            Bitmap resizedBitmap = Bitmap.createScaledBitmap(originalBitmap, targetWidth, targetHeight, true);
-                            Drawable drawable = new BitmapDrawable(getResources(), resizedBitmap);
-                            drawable.setBounds(0, 0, uploadPosterButton.getWidth(), uploadPosterButton.getHeight());
-                            uploadPosterButton.setBackground(drawable);
+                            float scaleWidth = uploadPosterButton.getWidth() / (float) originalBitmap.getWidth();
+                            float scaleHeight = uploadPosterButton.getHeight() / (float) originalBitmap.getHeight();
+                            Matrix matrix = new Matrix();
+                            matrix.setScale(scaleWidth, scaleHeight);
+                            BitmapShader shader = new BitmapShader(originalBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+                            shader.setLocalMatrix(matrix);
+                            ShapeDrawable shapeDrawable = new ShapeDrawable(new RoundRectShape(new float[] {
+                                    50, 50, 50, 50, 50, 50, 50, 50 }, null, null));
+                            shapeDrawable.getPaint().setShader(shader);
+                            int shapeDrawableWidth = Math.min(uploadPosterButton.getWidth(), uploadPosterButton.getWidth());
+                            int shapeDrawableHeight = Math.min(uploadPosterButton.getHeight(), uploadPosterButton.getHeight());
+                            shapeDrawable.setBounds(0, 0, shapeDrawableWidth, shapeDrawableHeight);
+                            uploadPosterButton.setBackground(shapeDrawable);
                             posterBox.setStartIconDrawable(null);
                             posterBox.setHint(null);
+                            posterBox.setBoxBackgroundMode(TextInputLayout.BOX_BACKGROUND_NONE);
+                            posterBox.setBackground(shapeDrawable);
+                            
 
                         } catch (FileNotFoundException e) {
                             e.printStackTrace();
