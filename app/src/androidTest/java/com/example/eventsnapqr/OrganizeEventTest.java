a12e7bfa9874710290f91ce488868c4674aa6145
Mjcs99 <maurius@ualberta.ca>
2024-04-07 20:32:55 -0600

    US 02.09.01 test done

--- app/src/androidTest/java/com/example/eventsnapqr/OrganizeEventTest.java
+++ app/src/androidTest/java/com/example/eventsnapqr/OrganizeEventTest.java
@@ -27,43 +27,7 @@ public class OrganizeEventTest {
     @Rule
     public ActivityScenarioRule<OrganizeAnEventActivity> scenario = new
             ActivityScenarioRule<OrganizeAnEventActivity>(OrganizeAnEventActivity.class);
-    /**
-     * Test to test that an event is successfully created
-     * US 01.01.01
 
-     @Test
-     public void organizeEventTest() {
-     FirebaseController firebaseController = new FirebaseController();
-     String id = firebaseController.getUniqueEventID();
-
-     // Launch OrganizeAnEventActivity and create the event
-     ActivityScenario.launch(OrganizeAnEventActivity.class);
-     onView(withId(R.id.editTextEventName)).perform(typeText(id));
-     onView(withId(R.id.edit_text_number)).perform(typeText("Event description"));
-
-     onView(withId(R.id.button)).perform(click());
-     // Use CountDownLatch to wait for Firebase operation to complete
-     CountDownLatch latch = new CountDownLatch(1);
-     firebaseController.getAllEvents(new FirebaseController.OnEventsLoadedListener() {
-     @Override
-     public void onEventsLoaded(ArrayList<Event> events) {
-     // Verify the event after it's loaded
-     for (Event event : events) {
-     if (event.getEventName().equals(id)) {
-     assertEquals(event.getEventName(), id);
-     latch.countDown(); // Signal that events are loaded
-     break;
-     }
-     }
-     }
-     });
-     try {
-     latch.await(10, TimeUnit.SECONDS); // Adjust timeout as needed
-     } catch (InterruptedException e) {
-     e.printStackTrace();
-     }
-     }
-     **/
     @Test
     public void geolocationTest(){
         // Launch OrganizeAnEventActivity and create the event
@@ -71,87 +35,7 @@ public class OrganizeEventTest {
         //onView(withId(R.id.editTextEventName)).perform(typeText(id));
         onView(withId(R.id.editTextDescription)).perform(typeText("Event description"));
     }
-    /**
-    @Before
-    public void init() {
-        Context context = InstrumentationRegistry.getInstrumentation().getContext();
-        ContentResolver contentResolver = context.getContentResolver();
-        String androidId = Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID);
-        // Disable animations
-        InstrumentationRegistry.getInstrumentation().getUiAutomation().executeShellCommand(
-                "settings put global window_animation_scale 0");
-        InstrumentationRegistry.getInstrumentation().getUiAutomation().executeShellCommand(
-                "settings put global transition_animation_scale 0");
-        InstrumentationRegistry.getInstrumentation().getUiAutomation().executeShellCommand(
-                "settings put global animator_duration_scale 0");
-        FirebaseController firebaseController = new FirebaseController();
-        CountDownLatch latch = new CountDownLatch(1);
-        final Boolean[] userExists = new Boolean[1];
 
-        FirebaseController.Authenticator listener = new FirebaseController.Authenticator() {
-            @Override
-            public void onUserExistenceChecked(boolean exists) {
-                if (exists) {
-                    userExists[0] = true;
-                    latch.countDown();
-                }
-                else {
-                    userExists[0] = false;
-                    latch.countDown();
-                }
-            }
-            @Override
-            public void onAdminExistenceChecked(boolean exists) {
-                // do nothing
-            }
-        };
-        FirebaseController.checkUserExists(androidId, listener);
-        try {
-            latch.await(10, TimeUnit.SECONDS); // Adjust timeout as needed
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        ActivityScenario.launch(MainActivity.class);
-
-        if(!userExists[0]){
-
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            onView(withId(R.id.edit_text_name)).perform(typeText("Test Event Name"));
-
-            onView(withId(R.id.edit_text_number)).perform(typeText("4033402450"));
-
-            onView(withId(R.id.edit_text_email)).perform(typeText("test@email.com"));
-
-            onView(withId(R.id.edit_text_homepage)).perform(typeText("www.homepage.com"));
-            onView(isRoot()).perform(ViewActions.closeSoftKeyboard());
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            onView(withId(R.id.button_sign_up)).perform(click());
-            FirebaseController.checkUserExists(androidId, listener);
-            try{
-                latch.await(8,TimeUnit.SECONDS);
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-            // Enable animations after the test is finished
-            InstrumentationRegistry.getInstrumentation().getUiAutomation().executeShellCommand(
-                    "settings put global window_animation_scale 1");
-            InstrumentationRegistry.getInstrumentation().getUiAutomation().executeShellCommand(
-                    "settings put global transition_animation_scale 1");
-            InstrumentationRegistry.getInstrumentation().getUiAutomation().executeShellCommand(
-                    "settings put global animator_duration_scale 1");
-        }
-    }
-**/
 
 }
 
